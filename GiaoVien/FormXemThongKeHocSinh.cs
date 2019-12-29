321fed4943ceaba31a3f7b7edb70e938e6b12333
La Chí Thành <thanhlahh999@gmail.com>
2019-12-30 01:07:53 +0700

    sửa form thống kê học sinh

--- GiaoVien/FormXemThongKeHocSinh.cs
+++ GiaoVien/FormXemThongKeHocSinh.cs
@@ -26,15 +26,112 @@ namespace GiaoVien
             if(lvShow.SelectedIndices.Count>0)
             {
                 lvChiTiet.Items.Clear();
-                txtTenHS.Text = lvShow.SelectedItems[0].SubItems[0].Text;
-                using (var qltn = Utils.QLTN.getInstance())
+                loadKyThi();
+                loadChiTiet();
+                cbbKyThi.SelectedIndexChanged += CBB_ChangedLoadChiTiet;
+            }
+        }
+
+        private void FormXemThongKeHocSinh_Load1(object sender, EventArgs e)
+        {
+            loadCapHoc();
+            LoadData();
+            cbKhoiHoc.SelectedIndexChanged += CBB_Changed; ;
+            
+        }
+
+        private void CBB_ChangedLoadChiTiet(object sender, EventArgs e)
+        {
+            if ((sender as ComboBox).SelectedIndex > -1)
+                loadChiTiet();
+        }
+        private void CBB_Changed(object sender, EventArgs e)
+        {
+            if ((sender as ComboBox).SelectedIndex > -1)
+                LoadData();
+        }
+        private void LoadData()
+        {
+            lvShow.Items.Clear();          
+            int cbb1 = cbKhoiHoc.SelectedIndex;
+            using (var qltn=Utils.QLTN.getInstance())
+            {
+                if (cbb1 == 0)
                 {
-                    int id = int.Parse(lvShow.SelectedItems[0].Text);
-                    var ds = qltn.HocSinhThamGias.Where(i=>i.hocsinhid==id && i.thoigianlambai.Length>0).
-                            Select(x => new {
+                    var dshs = qltn.TaiKhoans.Where(i => i.permission == 0).
+                        Select(x => new { x.hoten, x.LopHoc.tenlop, x.id });
+                    foreach (var item in dshs)
+                    {
+                        ListViewItem lvi = new ListViewItem();
+                        lvi.Text = item.id.ToString();
+                        lvi.SubItems.Add(item.hoten);
+                        lvi.SubItems.Add(item.tenlop);
+                        lvShow.Items.Add(lvi);
+                    }
+                }
+                else
+                {
+                    var dshs = qltn.TaiKhoans.Where(i => i.permission == 0&& i.LopHoc.caphocid==cbb1).
+                        Select(x => new { x.hoten, x.LopHoc.tenlop, x.id });
+                    foreach (var item in dshs)
+                    {
+                        ListViewItem lvi = new ListViewItem();
+                        lvi.Text = item.id.ToString();
+                        lvi.SubItems.Add(item.hoten);
+                        lvi.SubItems.Add(item.tenlop);
+                        lvShow.Items.Add(lvi);
+                    }
+                }
+            }
+            
+        }
+        void loadCapHoc()
+        {
+            cbKhoiHoc.Items.Clear();
+            cbKhoiHoc.Items.Add("Tất cả");
+            cbKhoiHoc.SelectedIndex = 0;
+            using (var qltn = Utils.QLTN.getInstance())
+            {
+                var dsCapHoc = qltn.CapHocs.ToList();
+                for (int i = 0; i < dsCapHoc.Count(); i++)
+                {
+                    cbKhoiHoc.Items.Add(dsCapHoc[i].tencap);
+                }
+            }
+        }
+
+        void loadKyThi()
+        {
+            cbbKyThi.Items.Clear();
+            cbbKyThi.Items.Add("Tất cả");
+            cbbKyThi.SelectedIndex = 0;
+            using (var qltn = Utils.QLTN.getInstance())
+            {
+                var dsKythi = qltn.KyThis.ToList();
+                for (int i = 0; i < dsKythi.Count(); i++)
+                {
+                    cbbKyThi.Items.Add(dsKythi[i].tenkythi);
+                }
+            }
+        }
+
+        void loadChiTiet()
+        {
+            lvChiTiet.Items.Clear();
+            txtTenHS.Text = lvShow.SelectedItems[0].SubItems[1].Text;
+            int cbb1 = cbbKyThi.SelectedIndex;
+            using (var qltn = Utils.QLTN.getInstance())
+            {
+                int id = int.Parse(lvShow.SelectedItems[0].Text);
+                if (cbb1 == 0)
+                {
+                    var ds = qltn.HocSinhThamGias.Where(i => i.hocsinhid == id && i.thoigianlambai.Length > 0).
+                            Select(x => new
+                            {
                                 x.DeThi.KyThi.tenkythi,
                                 x.DeThi.CapHoc_MonHoc.MonHoc.tenmonhoc,
-                                diem = (float)x.socaudung / x.DeThi.DeThi_CauHois.Count
+                                diem = (float)x.socaudung / x.DeThi.DeThi_CauHois.Count,
+                                loai = x.DeThi.KyThi.loaikythi,
                             });
                     foreach (var item in ds)
                     {
@@ -42,40 +139,51 @@ namespace GiaoVien
                         lvi.Text = item.tenkythi;
                         lvi.SubItems.Add(item.tenmonhoc);
                         lvi.SubItems.Add(item.diem.ToString());
+                        string loai = "";
+                        if(item.loai==false)
+                        {
+                            loai = "Thi thử";
+                        }
+                        else
+                        {
+                            loai = "Thi thật";
+                        }
+                        lvi.SubItems.Add(loai);                      
                         lvChiTiet.Items.Add(lvi);
                     }
-                    
                 }
-            }
-        }
-
-        private void FormXemThongKeHocSinh_Load1(object sender, EventArgs e)
-        {
-            LoadDataListShow();
-            LoadDataListChiTiet();
-        }
-
-        private void LoadDataListShow()
-        {
-            using (var qltn=Utils.QLTN.getInstance())
-            {
-                var dshs = qltn.TaiKhoans.Where(i => i.permission == 0).Select(x => new { x.hoten, x.LopHoc.tenlop,x.id });
-                foreach (var item in dshs)
+                else
                 {
-                    ListViewItem lvi = new ListViewItem();
-                    lvi.Text = item.id.ToString();
-                    lvi.SubItems.Add(item.hoten);
-                    lvi.SubItems.Add(item.tenlop);
-                    lvShow.Items.Add(lvi);
+                    var ds = qltn.HocSinhThamGias.Where(i => i.hocsinhid == id && i.thoigianlambai.Length > 0&& i.DeThi.KyThi.id==cbb1).
+                            Select(x => new
+                            {
+                                x.DeThi.KyThi.tenkythi,
+                                x.DeThi.CapHoc_MonHoc.MonHoc.tenmonhoc,
+                                diem = (float)x.socaudung / x.DeThi.DeThi_CauHois.Count,
+                                loai = x.DeThi.KyThi.loaikythi,
+                            });
+                    foreach (var item in ds)
+                    {
+                        ListViewItem lvi = new ListViewItem();
+                        lvi.Text = item.tenkythi;
+                        lvi.SubItems.Add(item.tenmonhoc);
+                        lvi.SubItems.Add(item.diem.ToString());
+                        string loai = "";
+                        if (item.loai == false)
+                        {
+                            loai = "Thi thử";
+                        }
+                        else
+                        {
+                            loai = "Thi thật";
+                        }
+                        lvi.SubItems.Add(loai);
+                        
+                        lvChiTiet.Items.Add(lvi);
+                    }
                 }
             }
-            
         }
-        private void LoadDataListChiTiet()
-        {
-
-        }
-
 
     }
 }
