feea0c5d4f63256892f7f6bc53ca4ab9643c764a
Long <thafnhlong@gmail.com>
2019-12-29 18:37:18 +0700

    Add waitingform

---
+++ Utils/frmWaiting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Utils
+{
+    public partial class frmWaiting : Form
+    {
+        public Action Worker { get; set; }
+        public frmWaiting(Action worker, string text = "Vui lòng chờ giây lát...")
+        {
+            InitializeComponent();
+            if (worker == null) { throw new ArgumentException(); }
+
+            Worker = worker;
+            label1.Text = text;
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            Task.Factory.StartNew(Worker).ContinueWith(t => { Close(); TaskScheduler.FromCurrentSynchronizationContext(); });
+        }
+    }
+}
